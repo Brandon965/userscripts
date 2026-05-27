eb43a9cd8029578aca937d282ad319c349ea13cc
Brandon965 <goofy9605@protonmail.com>
2026-05-27 17:04:24 -0500

    d

---
+++ mangabaka.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        MangaBaka Cover Help
+// @description Aids in uploading covers to MD
+// @namespace   https://github.com/Christopher-McGinnis
+// @author      Christopher McGinnis & Cwavs
+// @license     MIT
+// @icon        https://mangadex.org/favicon-96x96.png
+// @version     0.1.44
+// @include     /^(?:https?:\/\/)?.*bookwalker\.jp\/de[a-zA-Z0-9]+-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]+(\/.*)?/
+// @include     /^(?:https?:\/\/)?.*bookwalker\.jp\/series\/\d+(\/.*)?/
+// @include     /^(?:https?:\/\/)?mangadex\.org\/title\/\d+(\/.*)?/
+// @grant       GM_xmlhttpRequest
+
